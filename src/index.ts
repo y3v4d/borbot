cbc2a0d845de280523ae4a8e278bcdeb740e8bbf
y3v4d <bartekwethacz@gmail.com>
2022-04-26 03:01:09 +0200

    successful test mongodb connection, environemnt variables template

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,37 @@ import ready from './listeners/ready';
 import interactionCreate from './listeners/interactionCreate';
 import message from './listeners/message';
 import Bot from './core/bot';
+import mongoose from 'mongoose';
+
+interface IGuild {
+    guild_id: string,
+    admin_role: string,
+
+    user_uid: string,
+    password_hash: string
+}
+
+mongoose.connect(process.env.MONGODB_URI!).then(async () => {
+    console.log("Connected!");
+
+    const guildSchema = new mongoose.Schema<IGuild>({
+        guild_id: { type: String, required: true },
+        admin_role: { type: String, required: true },
+
+        user_uid: { type: String, required: true },
+        password_hash: { type: String, required: true }
+    });
+
+    const Guild = mongoose.model<IGuild>('Guild', guildSchema);
+
+    const guild = new Guild({
+        guild_id: "guild_id",
+        admin_role: "admin_role",
+
+        user_uid: "user_id",
+        password_hash: "password_hash"
+    });
+}).catch(error => console.error(error));
 
 console.log("Bot is starting...");
 const client = new Bot({
