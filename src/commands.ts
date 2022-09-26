e59bb0e77e6fa9691944a0dc5747288952209288
y3v4d <y3v4d@protonmail.com>
2022-09-26 14:53:14 +0200

    removed all unnecessary admin commands that were implemented in admin panel

--- src/commands.ts
+++ src/commands.ts
@@ -1,26 +1,10 @@
 import Command from "./core/command";
 import { TypeScript } from "./commands/typescript";
 import { Clan } from "./commands/clan";
-import { Connect } from "./commands/connect";
-import { Connected } from "./commands/connected";
-import { Schedule } from "./commands/schedule";
 import { Profile } from "./commands/profile";
-import { Setup } from "./commands/setup";
-import { MakeSchedule } from "./commands/make-schedule";
-import { SetupSchedule } from "./commands/setup-schedule";
-import { RemoveSchedule } from "./commands/remove-schedule";
-import { Disconnect } from "./commands/disconnect";
 
 export const Commands: Command[] = [
     TypeScript,
     Clan,
-    //Connect,
-    //Disconnect,
-    //Connected,
-    //Schedule,
-    Profile,
-    //MakeSchedule,
-    //Setup,
-    //SetupSchedule,
-    //RemoveSchedule
+    Profile
 ];
