519836cd36abe7930633d635adf0f25c5f1c75f9
y3v4d <y3v4d@protonmail.com>
2023-03-27 19:15:55 +0200

    ...

--- src/bot/client.ts
+++ src/bot/client.ts
@@ -162,6 +162,7 @@ export default class Bot extends Client {
     }
 
     get isDevelopment(): boolean {
-        return process.env.PRODUCTION != undefined;
+        console.log(`production ${process.env.PRODUCTION}`);
+        return process.env.PRODUCTION != 'true';
     }
 }
