5ade3a996b35172e4ecb4652e70c2bfd886def0a
y3v4d <y3v4d@protonmail.com>
2023-03-27 19:12:03 +0200

    temporary addition of PRODUCTION env

--- src/bot/client.ts
+++ src/bot/client.ts
@@ -162,7 +162,6 @@ export default class Bot extends Client {
     }
 
     get isDevelopment(): boolean {
-        console.log(`node env equal to `, process.env.NODE_ENV);
-        return process.env.NODE_ENV != "production";
+        return process.env.PRODUCTION != undefined;
     }
 }
